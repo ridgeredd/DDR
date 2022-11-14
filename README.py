85a222c05f9b08097dbe78a976687645041ba5c7
ridgeredd <113300015+ridgeredd@users.noreply.github.com>
2022-11-13 23:00:44 -0500

    made

---
+++ README.py
@@ -0,0 +1,30 @@
+# Ridge Redding usy7dp
+# West Plowman dzy5mz
+
+"""
+Game ideas:
+Dance Dance Revolution type game
+Directional arrows will drop down from the top of the screen
+The player must hit the corresponding arrow on their keyboard when the arrows hit a line at the bottom of the camera
+There will be a score tally and health meter at the side of the screen
+The closer you are to hitting the arrow right as it touches the line the more points you get
+Text will flash on the screen representing how close the player was to hitting it perfectly
+As you either miss, hit the wrong arrow button, or press a button too early, you will lose health
+If you hit enough consecutive arrow you start to slowly regain health
+Health regain increases as streak increases
+As time goes on, the arrows speed up
+If health drops to 0, the game ends
+Displays game over and top 5 players and their scores
+Gives you option to save your name and score to a high score file
+
+Basic Features:
+User Input: user inputs arrows corresponding to the ones on screen
+Game Over: games ends when health drops to 0
+Graphics: Arrows dropping down will be different colored graphics
+
+Additonal Features:
+Health Bar
+Inter-session Progress
+Timer Score
+Additional - health increases with displayed streak
+"""
